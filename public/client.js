1f5c94a94e64d003134dbf67cd02f21e4be5415c
haoua <haouasou@gmail.com>
2018-11-15 14:47:56 +0100

    json return

--- public/client.js
+++ public/client.js
@@ -1,12 +1,12 @@
 {
 	moment.locale('fr')
-	console.log(moment().format("hh:mm:ss"))
+	console.log(moment().format("HH:mm:ss"))
 
 	const vm = new Vue({
 		el: '#clock-vue',
 		template: `<h1>{{ time }}</h1>`,
 	    data: {
-	      time: moment().format("hh:mm:ss"),
+	      time: moment().format("HH:mm:ss"),
 	    },
 	  })
 }
--- time-server.js
+++ time-server.js
@@ -1,16 +1,36 @@
 const express = require('express')
 const app = express()
 
-
 const port = 4000
 
+const moment = require('moment')
+moment.locale('fr')
+
+var currentTime = moment().format("HH:mm:ss")
+
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/time-server.html')
+	if (req.accepts('json')) {
+		
+		const data =  {time: currentTime}
+	  	
+	  	res
+	    .header({
+	      'Content-Type': 'application/json',
+	    })
+	    .json(data)
+
+	} else if(req.accepts('text/html')) {
+
+	  res.sendFile(__dirname + '/public/time-server.html')
+	  
+	}
 })
 
 app.listen(port, () => {
   console.log(`Listening on ${port}`)
 })
+
 
 app.use(
   '/client',
